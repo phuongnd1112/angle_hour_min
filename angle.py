e27df61bfed4a4205541dff5a7d6497b6c16334f
NP Dang <63549500+phuongnd1112@users.noreply.github.com>
2020-11-13 13:51:35 +0000

    Update angle.py

--- angle.py
+++ angle.py
@@ -10,7 +10,7 @@ print(hour, minute)
 h_deg = 30
 m_deg = 6
 
-total_angle_hour = h_deg * hour + (15 * minute) / 30
+total_angle_hour = h_deg * hour + (15 * minute) / 30 #accounting for smaller changes impacted by minute, based on 15 degree shift every 30 minutes. 
 total_angle_minute = m_deg * minute 
 print(total_angle_hour, total_angle_minute)
 
