83bdcd7491570f9e56ab7a1b0f97ebf07b85b18d
NP Dang <63549500+phuongnd1112@users.noreply.github.com>
2020-11-13 13:49:17 +0000

    Add files via upload

---
+++ angle.py
@@ -0,0 +1,18 @@
+time = input('Time: ')
+print(time) 
+
+time_value = list(time.split(':'))
+print(time_value)
+hour = int(time_value[0])
+minute = int(time_value[1]) 
+print(hour, minute) 
+
+h_deg = 30
+m_deg = 6
+
+total_angle_hour = h_deg * hour + (15 * minute) / 30
+total_angle_minute = m_deg * minute 
+print(total_angle_hour, total_angle_minute)
+
+delta_angle = abs(total_angle_hour - total_angle_minute) 
+print(delta_angle) 
